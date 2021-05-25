1cbf902af6dc27e4560407614dcbb4b9bc677cd9
Armaan Majeed Khan <84519230+armaanmajeed@users.noreply.github.com>
2021-05-25 23:36:16 +0530

    File Updated

--- JS/hello.js
+++ JS/hello.js
@@ -1,1 +1,24 @@
-console.log("Heya Guys..!!")
+const monthlyIncome = 4000;
+const yearlyIncome = monthlyIncome * 12;
+// const means that you cannot change the value of variable in the next line.
+// let means that we can change the value the variable in the next line if we want to.
+
+console.log(monthlyIncome, yearlyIncome);
+
+const firstName = "Armaan";
+const middleName = "Majeed";
+const lastName = "Khan";
+
+const myName = "My name is " + firstName + " " + middleName + " " + lastName;
+const myNameTemplate = `My name is ${firstName} ${middleName} ${lastName}`;
+
+console.log(myName);
+console.log(myNameTemplate);
+
+const skyIsBlue = true;
+
+if (skyIsBlue) {
+    console.log("The sky is blue!");
+} else {
+    console.log("The sky is not blue!!");
+}
